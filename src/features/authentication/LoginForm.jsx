f3b9aeb2c0599f8c81edac5140036a6dd1853264
unknown <209410124@gme.tku.edu.tw>
2024-06-12 20:20:57 +0800

    update

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/Spinner';
 
 function LoginForm() {
-  const [email, setEmail] = useState('910226@gmail.com');
-  const [password, setPassword] = useState('910226');
+  const [email, setEmail] = useState('209410124@gmail.com');
+  const [password, setPassword] = useState('209410124');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
--- src/pages/Dashboard_24.jsx
+++ src/pages/Dashboard_24.jsx
@@ -10,8 +10,8 @@ function Dashboard() {
         <div>
           <li>Guset name :何書維</li>
           <li>Guset student ID :209410124</li>
-          <li>Guset email :910226@gmail.com</li>
-          <li>Supabase user id : dbea9a34-9680-478a-8ddd-ac915c00d41b</li>
+          <li>Guset email :209410124@gmail.com</li>
+          <li>Supabase user id : 86d883c9-b0d9-4136-897b-7336870fae8d</li>
         </div>
       </p>
     </Row>
