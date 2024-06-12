369b3d486c9cecad5aa9db51faf5bae42aef4367
unknown <209410124@gme.tku.edu.tw>
2024-06-12 20:10:03 +0800

    update

--- src/pages/Dashboard_24.jsx
+++ src/pages/Dashboard_24.jsx
@@ -1,19 +1,20 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
-import Uploader from '../data/Uploader'
-
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Dashboard() {
   return (
-    
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-     
-      
-      <p>TEST</p>
+    <Row>
+      <Heading as='h1'>Final-2 Exam of Web Prog2,209410124</Heading>
+
+      <p>
+        <div>
+          <li>Guset name :何書維</li>
+          <li>Guset student ID :209410124</li>
+          <li>Guset email :209410124@gms.tku.edu.tw</li>
+          <li>Supabase user id : dbea9a34-9680-478a-8ddd-ac915c00d41b</li>
+        </div>
+      </p>
     </Row>
-    
-    
   );
 }
 
